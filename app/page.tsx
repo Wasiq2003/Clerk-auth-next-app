31dbb5d8113bfcc7231014e10e0a865095c70738
Wasiq Ansari <wasiqansari.official@gmail.com>
2024-05-17 11:42:15 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className=' flex flex-col gap-4 h-screen w-full items-center justify-center'>
+      <h1 className='text-5xl'>
+        Home
+      </h1>
+      <Button asChild>
+        <Link href='/dashboard'>
+          Go to Dashboard
+        </Link>
+      </Button>
+    </div>
+  )
+}
+
+export default Home
---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+import { UserButton } from '@clerk/nextjs';
+import { UserResource } from '@clerk/types'
+import Link from 'next/link';
+
+interface HeaderProps {
+    user: UserResource | null | undefined;
+}
+
+const Header = ({ user }: HeaderProps) => {
+    return (
+        <div className=' bg-blue-400 h-[100px] text-white shadow-lg flex justify-between py-5 px-5 items-center'>
+            <div>
+                <h1>
+                    Logo
+                </h1>
+            </div>
+            <div className=' flex gap-2'>
+                <h1>
+                    Hi!
+                </h1>
+                <Link href='/profile'>
+                    {user?.username}
+                </Link>
+
+
+                <UserButton afterSignOutUrl='/' />
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
